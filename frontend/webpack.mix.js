940e547d01df97c8d075f03d688d6216224f4811
Mauro Oviedo <mauro.oviedo@protonmail.com>
2020-04-13 12:55:52 -0400

    Fixed baseDir

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -3,7 +3,7 @@ const mix = require('laravel-mix');
 require('laravel-mix-clean');
 
 
-const baseDir = '../home/static/member_portal/horizon';
+const baseDir = '../../home/static/member_portal/horizon';
 
 mix
   .js('./app/main.js', path.resolve(__dirname, `${baseDir}/javascripts/main.js`))
