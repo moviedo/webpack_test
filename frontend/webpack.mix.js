249a87c551c42dd26f053a688c4dc8132eeb0d38
Mauro Oviedo <mauro.oviedo@protonmail.com>
2020-04-14 14:34:52 -0400

    Final mix config.

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -1,11 +1,31 @@
-const path = require('path');
 const mix = require('laravel-mix');
+const CompressionPlugin = require('compression-webpack-plugin');
 require('laravel-mix-clean');
 
 
-const baseDir = '../../home/static/member_portal/horizon';
+mix.setPublicPath('../home/static/member_portal/horizon');
+mix.webpackConfig({
+  plugins: [new CompressionPlugin()],
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      }
+    ]
+  }
+});
 
 mix
-  .js('./app/main.js', path.resolve(__dirname, `${baseDir}/javascripts/main.js`))
+  .js('./app/main.js', 'javascripts/main.js')
+  .less('./app/main.less', 'styles/main.css')
+  .less('./app/vendor.less', 'styles/vendor.css')
   .extract()
-  .clean();
+  .clean()
+  .browserSync({
+    proxy: 'localhost:8000',
+    open: false
+  });
